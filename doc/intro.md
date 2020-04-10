8b91549165fdd6323d4a41960d04a531489dfe7a
LaVitaGrande <44675828+LaVitaGrande@users.noreply.github.com>
2020-04-10 12:27:10 -0400

    empty lein app

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to c-b-t
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/c_b_t/core_test.clj
@@ -0,0 +1,7 @@
+(ns c-b-t.core-test
+  (:require [clojure.test :refer :all]
+            [c-b-t.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
